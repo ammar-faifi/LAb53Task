1e1f745bc9da6d61725c15efde867aed7c9d5f69
Abdullah <abdullah@Macs-MacBook-Pro.local>
2023-02-07 15:53:30 +0300

    B here

--- Sorter.java
+++ Sorter.java
@@ -14,6 +14,34 @@ public class SelectionSortExample {
         }
     }
 
+public class InsertionSort {
+    void sort(int arr[]){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+                /* Move elements of arr[0..i-1], that are
+                   greater than key, to one position ahead
+                   of their current position */
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+    
+        static void printArray(int arr[])
+        {
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+     
+            System.out.println();
+        }
+
     public static void main(String a[]){
         int[] arr1 = {9,14,3,2,43,11,58,22};
         System.out.println("Before Selection Sort");
@@ -28,5 +56,12 @@ public class SelectionSortExample {
         for(int i:arr1){
             System.out.print(i+" ");
         }
+        int arr[] = { 12, 11, 13, 5, 6 };
+ 
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+ 
+        printArray(arr);
+
     }
 }
